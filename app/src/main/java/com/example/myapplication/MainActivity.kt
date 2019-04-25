b489811b902cf77c61c9a995514572ecdcf48191
BRX0001439 <externo.fabio.fehlberg@prosegur.com>
2019-04-25 09:15:01 -0300

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,44 @@
+package com.example.myapplication
+
+import android.content.Context
+import android.content.res.Configuration
+import android.os.Build
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
+
+
+class MainActivity : AppCompatActivity() {
+
+    override fun attachBaseContext(newBase: Context?) {
+        super.attachBaseContext(wrapContextLocale(newBase))
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        language.text = resources.configuration.locale.displayLanguage
+        country.text = resources.configuration.locale.displayCountry
+    }
+
+    private fun wrapContextLocale(context: Context?): Context? {
+        var ctx = context
+        val locale = Locale("es", "ar")
+        Locale.setDefault(locale)
+
+        val res = ctx?.resources
+        val config = Configuration(res?.configuration)
+
+        if (Build.VERSION.SDK_INT >= 17) {
+            config.setLocale(locale)
+            ctx = context?.createConfigurationContext(config)
+        } else {
+            config.locale = locale
+            res?.updateConfiguration(config, res.displayMetrics)
+        }
+
+        return ctx
+    }
+}
